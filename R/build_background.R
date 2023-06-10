18ff4ca7a998b6b6168b1c008cc788cfa3169827
Sebastian Boksem <sebastian.boksem@gmail.com>
2023-06-09 18:46:16 -0700

    added background and stickfigure functions

---
+++ R/build_background.R
@@ -0,0 +1,25 @@
+#' Returns a plot the background doodle
+#'
+#' @param ground Choose between grass, sand, or dirt ground
+#' @param num_cycles Choose between day and night for the sky.
+#'
+#' @return A background doodle
+#'
+#' @import ggplot2
+#'
+#' @export
+build_background <- function(ground = "grass", sky = "day"){
+  color_ground <- ifelse(ground == "grass", "green4",
+  ifelse(ground == "sand", "khaki1",
+  ifelse(ground == "dirt", "tan4", "green4")))
+
+  sky_color <- ifelse(sky == "day", "skyblue",
+                      ifelse(sky == "night", "darkblue", "skyblue"))
+  plot <-
+    ggplot2::ggplot() +
+    ggplot2::geom_rect(ggplot2::aes(xmin = -2, xmax = 2, ymin = -2, ymax = 0), fill = color_ground) +
+    ggplot2::geom_rect(ggplot2::aes(xmin = -2, xmax = 2, ymin = 0, ymax = 2), fill = sky_color) +
+    ggplot2::coord_equal(xlim = c(-1, 1), ylim = c(-1, 1)) +
+    ggplot2::theme_void()
+  plot
+}
---
+++ R/build_stick_figure.R
@@ -0,0 +1,31 @@
+#' Returns a plot the stick figure added
+#'
+#' @param background The previous background you have created
+#' @param scale The scale size of the stick figure, default is .25
+#' @param shift_x If you would want to shift the stick figure left or right, the minimum and maximum is -1 and 1.
+#' @param shift_y If you would want to shift the stick figure up or down, the minimum and maximum is -1 and 1.
+#'
+#' @return A stick figure
+#'
+#' @import ggplot2
+#'
+#' @export
+build_stick_figure <- function(background, scale = 0.25, shift_x = 0, shift_y = 0){
+  scale <- scale
+  radius <- 0.5 * scale
+  shift_x <- shift_x
+  shift_y <- shift_y
+  num_points <- 100
+  theta <- seq(0, 2 * pi, length.out = num_points)
+  x <- radius * cos(theta) + shift_x
+  y <- radius * sin(theta) + scale + scale/2 + shift_y
+  df <- data.frame(x = x, y = y)
+  background +
+    ggplot2::geom_segment(show.legend = FALSE,ggplot2::aes(x = -1 * scale + shift_x, y = -1 * scale + shift_y, xend = shift_x, yend = shift_y, size = 1)) +
+    ggplot2::geom_segment(show.legend = FALSE,ggplot2::aes(x = 1 * scale + shift_x, y = -1 * scale + shift_y, xend = shift_x, yend = shift_y, size = 1)) +
+    ggplot2::geom_segment(show.legend = FALSE,ggplot2::aes(x = shift_x, y = shift_y, xend = shift_x, yend = 1 * scale + shift_y, size = 1)) +
+    ggplot2::geom_segment(show.legend = FALSE,ggplot2::aes(x = -1 * scale + shift_x, y = shift_y, xend = shift_x, yend = 1 * scale + shift_y, size = 1)) +
+    ggplot2::geom_segment(show.legend = FALSE,ggplot2::aes(x = 1 * scale + shift_x, y = shift_y, xend = shift_x, yend = 1 * scale + shift_y, size = 1)) +
+    ggplot2::geom_path(data = df, show.legend = FALSE, ggplot2::aes(x, y, size = 1)) +
+    ggplot2::theme_void()
+}
